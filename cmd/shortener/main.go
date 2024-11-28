c370e0c8b6c835beaa4e00dfa4cd78b6b296209d
Painkiller675 <dimah1996@mail.ru>
2024-11-29 01:02:07 +0300

    iter3(v1.0) added chi in main.go

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/Painkiller675/url_shortener_675/internal/config"
 	"github.com/Painkiller675/url_shortener_675/internal/handlers"
+	"github.com/go-chi/chi/v5"
 	"net/http"
 )
 
@@ -11,12 +12,16 @@ func main() {
 	cfg := config.MustLoad()
 
 	// init router
-	mux := http.NewServeMux()
-	mux.HandleFunc("POST /", handlers.CreateShortURLHandler)
-	mux.HandleFunc("GET /{id}", handlers.GetLongURLHandler)
+	r := chi.NewRouter()
+
+	// routing
+	r.Route("/", func(r chi.Router) {
+		r.Post("/", handlers.CreateShortURLHandler)
+		r.Get("/{id}", handlers.GetLongURLHandler)
+	})
 
 	//start server
-	err := http.ListenAndServe(cfg.Address, mux)
+	err := http.ListenAndServe(cfg.Address, r)
 	if err != nil {
 		panic(err) // or log.Fatal()???
 
