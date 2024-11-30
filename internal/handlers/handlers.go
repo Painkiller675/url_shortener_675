4cabe8f26c2644593f6fa7c0a012123631f965d7
Painkiller675 <dimah1996@mail.ru>
2024-11-30 18:18:08 +0300

    iter4(v1.0)

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"github.com/Painkiller675/url_shortener_675/internal/config"
 	"github.com/Painkiller675/url_shortener_675/internal/repository"
 	"github.com/Painkiller675/url_shortener_675/internal/service"
 	"io"
@@ -28,7 +29,7 @@ func CreateShortURLHandler(res http.ResponseWriter, req *http.Request) {
 	randAl := service.GetRandString(8)
 	repository.WriteURL(randAl, string(body))
 	// response molding
-	baseURL := "http://localhost:8080/"
+	baseURL := config.StartOptions.BaseURL
 	resultURL, err := url.JoinPath(baseURL, randAl)
 	if err != nil {
 		http.Error(res, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
