f3638991c446a07480cb22324c0888c954bccaf4
Painkiller675 <dimah1996@mail.ru>
2024-11-30 11:56:53 +0300

    iter1(v2.5) no repository, just global value.

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,62 +1,64 @@
 package handlers
 
 import (
-	"fmt"
-	"github.com/Painkiller675/url_shortener_675/internal/repository"
 	"github.com/Painkiller675/url_shortener_675/internal/service"
 	"io"
+	"log"
 	"net/http"
 	"strconv"
-	"strings"
 )
+
+var origURL string
 
 func CreateShortURLHandler(res http.ResponseWriter, req *http.Request) {
 	// content checking
-	if !(strings.Contains(req.Header.Get("Content-Type"), "text/plain")) {
-		http.Error(res, http.StatusText(http.StatusBadRequest), http.StatusBadRequest) //400
-		return
-	}
+	//if !(strings.Contains(req.Header.Get("Content-Type"), "text/plain")) {
+	//	http.Error(res, http.StatusText(http.StatusBadRequest), http.StatusBadRequest) //400
+	//	return
+	//}
 	// body checking
 	body, err := io.ReadAll(req.Body)
 	if err != nil || len(body) == 0 {
 		http.Error(res, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
 		return
 	}
+	origURL = string(body)
 	// write alias
 	// TODO mb GetRandURL should return error too?
-	randURL := service.GetRandURL(8, string(body))
-	repository.WriteURL(string(body), randURL)
+	randURL := service.GetRandURL(8, origURL)
+	//repository.WriteURL(string(body), randURL)
 	// response molding
 	res.Header().Set("Content-Type", "text/plain")
-	res.Header().Set("Content-Length", strconv.Itoa(len(randURL)))
+	res.Header().Set("Content-Length", strconv.Itoa(len([]byte(randURL))))
 	// TODO add content length?
 	res.WriteHeader(http.StatusCreated) // 201
 	_, err = res.Write([]byte(randURL))
 	if err != nil {
-		http.Error(res, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		log.Printf("Error writing to response: %v", err)
 		return
 	}
 }
 
 func GetLongURLHandler(res http.ResponseWriter, req *http.Request) {
 	// content checking
-	if !(strings.Contains(req.Header.Get("Content-Type"), "text/plain")) {
-		http.Error(res, http.StatusText(http.StatusBadRequest), http.StatusBadRequest) //400
-		return
-	}
-	id := req.PathValue("id")
+	//if !(strings.Contains(req.Header.Get("Content-Type"), "text/plain")) {
+	//	http.Error(res, http.StatusText(http.StatusBadRequest), http.StatusBadRequest) //400
+	//	return
+	//}
+	id := req.PathValue("id") // the cap
+	_ = id                    // the cap
 	//if id == "" {
 	//	http.Error(res, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
 	//	return
 	//}
 	// response molding
-	url, err := repository.GetShortURL(id)
-	fmt.Println("REQUEST: ", id)
-	fmt.Println("REQUEST: ", url)
-	if err != nil { //TODO: mb change that to error 400>?
-		http.Error(res, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
-		return
-	}
-	res.Header().Set("Location", url)
+	//url, err := repository.GetShortURL(id)
+	//fmt.Println("REQUEST: ", id)
+	//fmt.Println("REQUEST: ", url)
+	//if err != nil { //TODO: mb change that to error 400>?
+	//	http.Error(res, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+	//	return
+	//}
+	res.Header().Set("Location", origURL)
 	res.WriteHeader(http.StatusTemporaryRedirect) // 307
 }
