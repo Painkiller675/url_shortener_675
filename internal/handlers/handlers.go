1ef5f11ce9bf5d82b70dee0bece5e05d34522245
Painkiller675 <dimah1996@mail.ru>
2024-11-30 16:18:42 +0300

    iter1(v2.7) try to make safeStruct - useful!

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -5,17 +5,17 @@ import (
 	"github.com/Painkiller675/url_shortener_675/internal/repository"
 	"github.com/Painkiller675/url_shortener_675/internal/service"
 	"io"
+	"log"
 	"net/http"
 	"strconv"
-	"strings"
 )
 
 func CreateShortURLHandler(res http.ResponseWriter, req *http.Request) {
 	// content checking
-	if !(strings.Contains(req.Header.Get("Content-Type"), "text/plain")) {
-		http.Error(res, http.StatusText(http.StatusBadRequest), http.StatusBadRequest) //400
-		return
-	}
+	//if !(strings.Contains(req.Header.Get("Content-Type"), "text/plain")) {
+	//	http.Error(res, http.StatusText(http.StatusBadRequest), http.StatusBadRequest) //400
+	//	return
+	//}
 	// body checking
 	body, err := io.ReadAll(req.Body)
 	if err != nil || len(body) == 0 {
@@ -24,26 +24,27 @@ func CreateShortURLHandler(res http.ResponseWriter, req *http.Request) {
 	}
 	// write alias
 	// TODO mb GetRandURL should return error too?
-	randURL := service.GetRandURL(8, string(body))
-	repository.WriteURL(string(body), randURL)
+	randAl := service.GetRandString(8)
+	repository.WriteURL(randAl, string(body))
 	// response molding
 	res.Header().Set("Content-Type", "text/plain")
-	res.Header().Set("Content-Length", strconv.Itoa(len(randURL)))
+	res.Header().Set("Content-Length", strconv.Itoa(len([]byte(randAl))))
 	// TODO add content length?
 	res.WriteHeader(http.StatusCreated) // 201
-	_, err = res.Write([]byte(randURL))
-	if err != nil {
-		http.Error(res, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+	_, err = res.Write([]byte(randAl))
+	if err != nil { // bbrowser can't show 2 statuses
+		log.Printf("Error writing to response: %v", err)
+		//http.Error(res, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 		return
 	}
 }
 
 func GetLongURLHandler(res http.ResponseWriter, req *http.Request) {
 	// content checking
-	if !(strings.Contains(req.Header.Get("Content-Type"), "text/plain")) {
-		http.Error(res, http.StatusText(http.StatusBadRequest), http.StatusBadRequest) //400
-		return
-	}
+	//if !(strings.Contains(req.Header.Get("Content-Type"), "text/plain")) {
+	//	http.Error(res, http.StatusText(http.StatusBadRequest), http.StatusBadRequest) //400
+	//	return
+	//}
 	id := req.PathValue("id")
 	//if id == "" {
 	//	http.Error(res, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -2,36 +2,119 @@ package repository
 
 import (
 	"fmt"
-	"strings"
+	"sync"
 )
 
-var URLStorage = make(map[string]string) // ALIAS - URL
+type safeStruct struct { // TODO mb create 2 structures with diff mutexes??
+	aliasOrURL map[string]string // 1, mostly for GET
+	orURLAlias map[string]string // 2, mostly for POST
+	mx1        sync.RWMutex
+	mx2        sync.RWMutex
+}
 
-func WriteURL(url string, alias string) {
-	isChanged := false
+func (s *safeStruct) storeValue1(alias string, orURL string) {
+	//s.mx1.Lock() //TODO mb 1st I should check everything nd then use mx?
+	//defer s.mx1.Unlock()
+
+	s.aliasOrURL[alias] = orURL
+	//s.mx1.Unlock()
+}
+
+func (s *safeStruct) getValue1(alias string) string {
+	//s.mx1.RLock()
+	//defer s.mx1.RUnlock()
+	if orURL, ok := s.aliasOrURL[alias]; ok {
+		s.mx1.RUnlock()
+		return orURL
+	}
+	//s.mx1.RUnlock()
+	return "" // TODO add error type?
+}
+
+func (s *safeStruct) del1(alias string) {
+	//s.mx1.RLock()
+	//defer s.mx1.RUnlock()
+
+	delete(s.aliasOrURL, alias)
+	//s.mx1.Unlock()
+}
+func (s *safeStruct) storeValue2(alias string, orURL string) {
+	//s.mx2.Lock() //TODO mb 1st I should check everything nd then use mx?
+	//defer s.mx2.Unlock()
+
+	s.orURLAlias[alias] = orURL
+	//s.mx2.Unlock()
+}
+
+func (s *safeStruct) getValue2(orURL string) string {
+	//s.mx2.RLock()
+	//defer s.mx2.RUnlock()
+	if alias, ok := s.orURLAlias[orURL]; ok {
+		s.mx2.RUnlock()
+		return alias
+	}
+	//s.mx2.RUnlock()
+	return ""
+}
+
+func (s *safeStruct) StoreValue1(alias string, orURL string) {
+	s.mx1.Lock()
+	defer s.mx1.Unlock()
+	s.storeValue1(alias, orURL)
+}
+
+func (s *safeStruct) StoreValue2(orURL string, alias string) {
+	s.mx1.Lock()
+	defer s.mx1.Unlock()
+	s.storeValue2(orURL, alias)
+}
+
+func (s *safeStruct) GetValue1(alias string) string {
+	s.mx1.Lock()
+	defer s.mx1.Unlock()
+	return s.getValue1(alias)
+}
+
+func (s *safeStruct) GetValue2(orURL string) string {
+	s.mx1.Lock()
+	defer s.mx1.Unlock()
+	return s.getValue2(orURL)
+}
+
+func (s *safeStruct) Del1(alias string) {
+	s.mx1.Lock()
+	defer s.mx1.Unlock()
+	s.del1(alias)
+}
+
+var URLStorage safeStruct
+
+func WriteURL(newAl string, newOrURL string) {
 	// check if such url already exists if exists => change that
-	for al, ur := range URLStorage {
-		if ur == url {
-			delete(URLStorage, al)
-			URLStorage[alias] = url
-			isChanged = true
-			break
-		}
+	curAl := URLStorage.GetValue2(newOrURL)
+	if curAl != "" {
+		URLStorage.Del1(curAl)
+		URLStorage.StoreValue1(newAl, newOrURL)
+		URLStorage.StoreValue2(newOrURL, newAl)
+		fmt.Println("FROM WriteURL (exists):")
+		fmt.Println(URLStorage.aliasOrURL)
+		fmt.Println(URLStorage.orURLAlias)
+	} else { // just store a new orURL cause that's smth new
+		URLStorage.StoreValue1(newAl, newOrURL)
+		URLStorage.StoreValue2(newOrURL, newAl)
+		fmt.Println("FROM WriteURL (add):")
+		fmt.Println(URLStorage.aliasOrURL)
+		fmt.Println(URLStorage.orURLAlias)
 	}
-	// if not exists => add
-	if !isChanged {
-		URLStorage[alias] = url
-	}
-	fmt.Println("from write", URLStorage)
 }
 
 func GetShortURL(alias string) (string, error) {
-	for al := range URLStorage {
-		if strings.Contains(al, alias) {
-			return al, nil
-		}
+	curOrURL := URLStorage.GetValue1(alias)
+	fmt.Println("FROM GET, curOrURL =", curOrURL)
+	if curOrURL == "" {
+		return "", fmt.Errorf("%v does not exist", alias)
 	}
-	return "", fmt.Errorf("%v does not exist", alias)
+	return curOrURL, nil
 }
 
 //url, isExist := UrlStorage[alias]
